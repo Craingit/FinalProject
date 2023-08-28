6313c886a9615e2af3bed93ad16ae501cfb1414a
Crain <Crain@DESKTOP-N2NI9UI>
2023-08-29 02:24:11 +0300

    Proje dosyası ekle.

---
+++ FinalProject/Controllers/PaymentController.cs
@@ -0,0 +1,38 @@
+﻿// PaymentController.cs
+
+using Microsoft.AspNetCore.Mvc;
+
+public class PaymentController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult ProcessPayment()
+    {
+        try
+        {
+            // Simulate payment processing using MyPaymentGateway (replace with actual gateway)
+            // You would typically send payment data and handle the response here.
+
+            // If payment is successful, update the user's premium status
+            // Example: user.IsPremium = true;
+
+            // Redirect to a thank you page or any appropriate page
+            return RedirectToAction("ThankYou");
+        }
+        catch (Exception ex)
+        {
+            // Handle payment errors gracefully and provide user feedback
+            ViewBag.Error = "Payment failed. Please try again.";
+            return View("Index");
+        }
+    }
+
+    public IActionResult ThankYou()
+    {
+        return View();
+    }
+}
